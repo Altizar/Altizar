909cc296af64f1fa1f56a0d8f250bfe05b50ad24
Altizar <altizar_23@hotmail.com>
2016-07-31 22:00:26 -0700

    mdo - update names

---
+++ MiddenQuest.Inventory.Tier.Namer.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         MidenQuest - Item Tier Namer
+// @namespace    https://github.com/Altizar/Altizar.github.io
+// @version      0.1
+// @description  MidenQuest Item Tier Namer
+// @author       Altizar
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
+// @include      http://www.midenquest.com/Game.aspx
+// @include      http://midenquest.com/Game.aspx
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==/* 
+
+var jA = document.createElement('script');
+jA.setAttribute('type', 'text/javascript');
+jA.setAttribute('src', 'https://altizar.github.io/MiddenQuest.Inventory.Tier.Namer.Code.js');
+document.body.appendChild(jA);
