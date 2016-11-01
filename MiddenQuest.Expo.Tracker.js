44b67e91175317f126a07a84cf6f526fb52473f9
Altizar <altizar_23@hotmail.com>
2016-11-01 15:24:43 -0700

    Updated tracker script to do navigation expos

--- MiddenQuest.Expo.Tracker.js
+++ MiddenQuest.Expo.Tracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MidenQuest - Expo Tracker
 // @namespace    https://github.com/Altizar/Altizar.github.io
-// @version      0.5
+// @version      0.6
 // @description  MidenQuest - Market Tracker
 // @author       Altizar
 // @include      http://www.midenquest.com/Game.aspx
@@ -15,8 +15,8 @@ var expo_finished = 0;
 var expo_navigate = 0;
 var updateExpoTimeoutEvent = 0;
 var updateExpoNavigateTimeoutEvent = 0;
-var message = "Check Now";
-var message_2 = "Check Now";
+var message = "<span style='color:blue;cursor: pointer;' onClick=\"sendRequestContentFill('getExpedition.aspx?null='); fightengaged = 0;\">Check Now</div>";
+var message_2 = "<span style='color:blue;cursor: pointer;' onClick=\"sendRequestContentFill('getNavigation.aspx?null='); fightengaged = 0;\">Check Now</div>";
 var target = document.getElementById('ContentLoad');
 var config = {attributes: true, childList: true, characterData: true};
 
@@ -61,10 +61,11 @@ function updateExpoTimeout() {
             clearInterval(updateExpoTimeoutEvent);
             updateExpoTimeoutEvent = 0;
         }
-        message = "Finished";
+        message = "<span style='color:blue;cursor: pointer;' onClick=\"sendRequestContentFill('getExpedition.aspx?null='); fightengaged = 0;\">Finished</div>";
+        message = "";
         expo_finished = 0;
     }
-    jQuery('#expo_timer').text(message);
+    jQuery('#expo_timer').html(message);
 }
 function updateExpoNavigateTimeout() {
     var curTime = jQuery.now();
@@ -74,19 +75,19 @@ function updateExpoNavigateTimeout() {
         myDate.setHours(0);
         myDate.setSeconds(timeLeft);
         if (myDate.getHours() > 0) {
-            message = myDate.getHours() + 'hr ' + myDate.getMinutes() + 'min';
+            message_2 = myDate.getHours() + 'hr ' + myDate.getMinutes() + 'min';
         } else {
-            message = myDate.getMinutes() + 'min';
+            message_2 = myDate.getMinutes() + 'min';
         }
     } else if (expo_finished > 0) {
         if (updateExpoNavigateTimeoutEvent !== 0) {
             clearInterval(updateExpoNavigateTimeoutEvent);
             updateExpoNavigateTimeoutEvent = 0;
         }
-        message = "Finished";
+        message_2 = "<span style='color:blue;cursor: pointer;' onClick=\"sendRequestContentFill('getNavigation.aspx?null='); fightengaged = 0;\">Finished</div>";
         expo_navigate = 0;
     }
-    jQuery('#expo_timer_2').text(message);
+    jQuery('#expo_timer_2').html(message_2);
 }
 var observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
@@ -99,6 +100,8 @@ var observer = new MutationObserver(function (mutations) {
             }
         }
     });
+    jQuery('#expo_timer_2').html(message_2);
+    jQuery('#expo_timer').html(message);
 });
 observer.observe(target, config);
 
