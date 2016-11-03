0d44b71d76991a0e00cf9da52141069979185209
Altizar <altizar_23@hotmail.com>
2016-11-02 21:10:15 -0700

    Midden Quest Expo Tools Update pt7

--- MiddenQuest.Expo.Send.Highlighter.js
+++ MiddenQuest.Expo.Send.Highlighter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MidenQuest - Expo Send Highlighter
 // @namespace    https://github.com/Altizar/Altizar.github.io
-// @version      0.1
+// @version      0.2
 // @description  MidenQuest - Expo Send Highlighter
 // @author       Altizar
 // @include      http://www.midenquest.com/Game.aspx
@@ -17,6 +17,22 @@ var MQO_Expo_Button_Highlighter = {
         attributes: true,
         childList: true,
         characterData: true
+    },
+    best: {
+        T1: {name: 'None', value: 0},
+        T2: {name: 'None', value: 0},
+        T3: {name: 'None', value: 0},
+        T4: {name: 'None', value: 0},
+        T5: {name: 'None', value: 0}
+    },
+    getBest: function (tier) {
+        return this.best[tier].name;
+    },
+    setBest: function (tier, name, value) {
+        if (this.best[tier].value < value) {
+            this.best[tier].value = value;
+            this.best[tier].name = name;
+        }
     },
     stock: {
         Ore_1: 0,
@@ -62,6 +78,8 @@ var MQO_Expo_Button_Highlighter = {
         jQuery('#ContentLoad button').each(function () {
             var text = jQuery(this).text().match(/([^T]*)[T]([0-5])[ ](\w*)/);
             if (text !== null) {
+                var best = MQO_Expo_Button_Highlighter.getBest('T' + text[2]);
+                console.log(best);
                 var name = text[3] + '_' + text[2];
                 var stockNeed = MQO_Expo_Button_Highlighter.parse_value(text[1]);
                 var stockOwn = MQO_Expo_Button_Highlighter.stock[name];
@@ -70,6 +88,9 @@ var MQO_Expo_Button_Highlighter = {
                 }
                 if (stockOwn < stockNeed) {
                     jQuery(this).removeClass("ui-state-secondary").addClass("ui-state-error").unbind('click');
+                }
+                if (text[3] === best) {
+                    jQuery(this).removeClass("ui-state-default").removeClass("darkBtn").addClass("ui-state-highlight");
                 }
             }
         });
@@ -95,6 +116,26 @@ var MQO_Expo_Button_Highlighter = {
         this.stock.Fish_3 = this.getValue('T3FishDesc');
         this.stock.Fish_4 = this.getValue('T4FishDesc');
         this.stock.Fish_5 = this.getValue('T5FishDesc');
+        this.setBest('T1', 'Ore', this.stock.Ore_1);
+        this.setBest('T2', 'Ore', this.stock.Ore_2);
+        this.setBest('T3', 'Ore', this.stock.Ore_3);
+        this.setBest('T4', 'Ore', this.stock.Ore_4);
+        this.setBest('T5', 'Ore', this.stock.Ore_5);
+        this.setBest('T1', 'Plant', this.stock.Plant_1);
+        this.setBest('T2', 'Plant', this.stock.Plant_2);
+        this.setBest('T3', 'Plant', this.stock.Plant_3);
+        this.setBest('T4', 'Plant', this.stock.Plant_4);
+        this.setBest('T5', 'Plant', this.stock.Plant_5);
+        this.setBest('T1', 'Wood', this.stock.Wood_1);
+        this.setBest('T2', 'Wood', this.stock.Wood_2);
+        this.setBest('T3', 'Wood', this.stock.Wood_3);
+        this.setBest('T4', 'Wood', this.stock.Wood_4);
+        this.setBest('T5', 'Wood', this.stock.Wood_5);
+        this.setBest('T1', 'Fish', this.stock.Fish_1);
+        this.setBest('T2', 'Fish', this.stock.Fish_2);
+        this.setBest('T3', 'Fish', this.stock.Fish_3);
+        this.setBest('T4', 'Fish', this.stock.Fish_4);
+        this.setBest('T5', 'Fish', this.stock.Fish_5);
     },
     start: function () {
         var observer = new MutationObserver(function (mutations) {
