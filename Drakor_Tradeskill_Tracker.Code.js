4617f79b12ac2571fb358e931cfeb7799d13d823
Altizar <altizar_23@hotmail.com>
2017-02-11 17:03:55 +0000

    bug fix

--- Drakor_Tradeskill_Tracker.Code.js
+++ Drakor_Tradeskill_Tracker.Code.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Drakor TS Tracker
 // @namespace    https://github.com/Altizar/Altizar.github.io
-// @version      0.1.7
+// @version      0.1.8
 // @description  Tracks statistics of tradeskills
 // @description  MidenQuest - Expo Send Highlighter
 // @author       Altizar
@@ -132,7 +132,7 @@ var Drakor_Tradeskill_Tracker = {
         Drakor_Tradeskill_Tracker.drawTable(type);
         var table = '';
         var firstRow = true;
-        Object.keys(Drakor_Tradeskill_Tracker.data.Fishing["Great Barrier Peak"].catches).sort().forEach(function (key) {
+        Object.keys(Drakor_Tradeskill_Tracker.data[type][location].catches).sort().forEach(function (key) {
             if (firstRow) {
                 table += '<tr><td colspan="3" style="lineheight=2px;">&nbsp;</td></tr>';
                 firstRow = false;
